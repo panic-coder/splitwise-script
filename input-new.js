3c8ad278b6132a76bd710b42c0d4a34143432a62
panic-coder <androidshubham@gmail.com>
2019-07-02 07:58:59 +0530

    changes as per as hacker earth

---
+++ input-new.js
@@ -0,0 +1,145 @@
+module.exports = {
+    "input": [{
+        "paidBy": "A",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "0"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            }, {
+                "user": "C",
+                "paid": "300"
+            },
+            {
+                "user": "D",
+                "paid": "35"
+            },
+            {
+                "user": "E",
+                "paid": "35"
+            }
+        ]
+    }, {
+        "paidBy": "B",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "0"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            },
+            {
+                "user": "C",
+                "paid": "0"
+            },
+            {
+                "user": "D",
+                "paid": "40"
+            },
+            {
+                "user": "E",
+                "paid": "10"
+            }
+        ]
+    },
+    {
+        "paidBy": "B",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "0"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            },
+            {
+                "user": "C",
+                "paid": "10"
+            },
+            {
+                "user": "D",
+                "paid": "0"
+            },
+            {
+                "user": "E",
+                "paid": "0"
+            }]
+    }, {
+        "paidBy": "C",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "0"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            },
+            {
+                "user": "C",
+                "paid": "0"
+            },
+            {
+                "user": "D",
+                "paid": "10"
+            },
+            {
+                "user": "E",
+                "paid": "0"
+            }
+        ]
+    }, {
+        "paidBy": "D",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "0"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            },
+            {
+                "user": "C",
+                "paid": "0"
+            },
+            {
+                "user": "D",
+                "paid": "0"
+            },
+            {
+                "user": "E",
+                "paid": "10"
+            }]
+    }, {
+        "paidBy": "E",
+        "paidFor": [
+            {
+                "user": "A",
+                "paid": "10"
+            },
+            {
+                "user": "B",
+                "paid": "0"
+            },
+            {
+                "user": "C",
+                "paid": "0"
+            },
+            {
+                "user": "D",
+                "paid": "0"
+            },
+            {
+                "user": "E",
+                "paid": "0"
+            }
+        ]
+    }]
+};
